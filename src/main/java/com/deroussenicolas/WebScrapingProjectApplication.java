c82539505d2c6ab4e416eba30f1906cc1eff2d09
silverawz <deroussenicolas@gmail.com>
2021-02-09 16:36:06 +0100

    first commit webscraping

---
+++ src/main/java/com/deroussenicolas/WebScrapingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deroussenicolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deroussenicolas/WebScrapingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deroussenicolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
